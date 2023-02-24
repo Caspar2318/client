35b893b8726929eaae51bf383c6226d98dfd7985
Caspar2318 <108163945+Caspar2318@users.noreply.github.com>
2023-02-25 08:34:35 +1000

    update

--- src/components/charts/PropertyReferrals.tsx
+++ src/components/charts/PropertyReferrals.tsx
@@ -7,11 +7,34 @@ interface ProgressBarProps {
   color: string;
 }
 
-const ProgressBar = ({ title, percentage, color }: ProgressBarProps) => {
+const ProgressBar = ({ title, percentage, color }: ProgressBarProps) => (
   <Box width="100%">
-    <Stack direction="row"></Stack>
-  </Box>;
-};
+    <Stack direction="row" alignItems="center" justifyContent="space-between">
+      <Typography fontSize={16} fontWeight={500} color="#11142d">
+        {title}
+      </Typography>
+      <Typography fontSize={16} fontWeight={500} color="#11142d">
+        {percentage}
+      </Typography>
+    </Stack>
+    <Box
+      mt={2}
+      position="relative"
+      width="100%"
+      height="8px"
+      borderRadius={1}
+      bgcolor="#e4e8ef"
+    >
+      <Box
+        width={`${percentage}%`}
+        bgcolor={color}
+        position="absolute"
+        height="100%"
+        borderRadius={1}
+      />
+    </Box>
+  </Box>
+);
 
 const PropertyReferrals = () => {
   return (
@@ -28,7 +51,11 @@ const PropertyReferrals = () => {
         Property Referrals
       </Typography>
 
-      <Stack my="20px" direction="column" gap={4}></Stack>
+      <Stack my="20px" direction="column" gap={4}>
+        {propertyReferralsInfo.map((bar) => (
+          <ProgressBar key={bar.title} {...bar} />
+        ))}
+      </Stack>
     </Box>
   );
 };
