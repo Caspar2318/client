33129ff7fda838aff8c3e37a772adbba681db804
Caspar2318 <108163945+Caspar2318@users.noreply.github.com>
2023-02-26 15:31:47 +1000

    property functions

--- src/components/common/CustomButton.tsx
+++ src/components/common/CustomButton.tsx
@@ -9,9 +9,12 @@ const CustomButton = ({
   fullWidth,
   icon,
   handleClick,
+  disabled,
 }: CustomButtonProps) => {
   return (
     <Button
+      disabled={disabled}
+      type={type === "submit" ? "submit" : "button"}
       sx={{
         flex: fullWidth ? 1 : "unset",
         padding: "10px 15px",
